b200bbfc65ca0c61841fa0ee75248c58e9092971
rue-22 <cssalces@up.edu.ph>
2023-12-26 20:02:10 +0800

    feat: implement a simple page that contains details about me

---
+++ app.js
@@ -0,0 +1,27 @@
+// button.addEventListener("click", function () {
+//     if (nameAnswer.classList.contains("reveal")) {
+//         nameAnswer.classList.remove("reveal");
+//     } else {
+//         nameAnswer.classList.add("reveal");
+//     }
+// });
+
+const ansElements = document.querySelectorAll(
+    "#name-ans, #age-ans, #study-ans, #lose-ans"
+);
+const container = document.querySelector("#container");
+
+container.addEventListener("click", function (e) {
+    console.log(e);
+    if (e.target.classList.contains("btn")) {
+        const button = e.target;
+        const answerId = button.name;
+        const actualAnswer = document.getElementById(answerId);
+
+        if (actualAnswer.classList.contains("reveal")) {
+            actualAnswer.classList.remove("reveal");
+        } else {
+            actualAnswer.classList.add("reveal");
+        }
+    }
+});
